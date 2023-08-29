a817f73e99a678a58adc989814dd7575e8c565fc
KaiZerX000 <supsup2546@gmail.com>
2023-08-30 01:17:50 +0700

    30/08/2566

--- pages/index.js
+++ pages/index.js
@@ -1,147 +1,109 @@
-import React from 'react'
-import Head from 'next/head'
-import Image from 'next/image';
+import { useSession, signIn, signOut } from "next-auth/react"
+import Image from "next/image";
 import Link from 'next/link';
+import { useRouter } from "next/router";
 
-export default function index() {
-  return (
-    <>
-    <Head>
-      <title>หน้าแรก</title>
-    </Head>
-  
-  <header>
-<nav className="navbar navbar-expand-lg bg-info"> 
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" href="/.">หน้าแรก</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" href="/about">เกี่ยวกับเรา</Link>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            บริการของเรา
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">1</a></li>
-            <li><a className="dropdown-item" href="#">2</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" href="/contact"> ติดต่อเรา</Link>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-dark" type="submit">ค้นหา</button>
-      </form>
-    </div>
-  </div>
-</nav>
+export async function getServerSideProps() {
+  const res = await fetch('https://6fc7-184-82-26-219.ngrok-free.app/api/users')
+  const posts = await res.json();
 
-<div>
-
-<div id="carouselExampleInterval" classname="carousel slide" data-bs-ride="carousel">
-  <div classname="carousel-inner">
-    <div className="carousel-item active" data-bs-interval={1000}>
-      <img src="/panda.jpg" className="d-block w-100" alt="cow" />
-    </div>
-    <div className="carousel-item" data-bs-interval={1000}>
-      <img src="/panda2.png" className="d-block w-100" alt="chick" />
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true" />
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true" />
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-
-
-
-</div>
-</header>
-<br />
-<br />
-<br /> 
-<main>
-
-
-</main>
-<br />
-<br />
-<br />
-
-<footer>
-
-<div className="container">
-  <footer className="py-5">
-    <div className="row">
-      <div className="col-6 col-md-2 mb-3">
-        <h5>Section</h5>
-        <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-        </ul>
-      </div>
-      <div className="col-6 col-md-2 mb-3">
-        <h5>Section</h5>
-        <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-        </ul>
-      </div>
-      <div className="col-6 col-md-2 mb-3">
-        <h5>Section</h5>
-        <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-        </ul>
-      </div>
-      <div className="col-md-5 offset-md-1 mb-3">
-        <form>
-          <h5>Subscribe to our newsletter</h5>
-          <p>Monthly digest of what's new and exciting from us.</p>
-          <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-            <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
-            <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
-            <button className="btn btn-primary" type="button">Subscribe</button>
-          </div>
-        </form>
-      </div>
-    </div>
-    <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-      <p>© 2023 Company, Inc. All rights reserved.</p>
-      <ul className="list-unstyled d-flex">
-        <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#twitter" /></svg></a></li>
-        <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#instagram" /></svg></a></li>
-        <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width={24} height={24}><use xlinkHref="#facebook" /></svg></a></li>
-      </ul>
-    </div>
-  </footer>
-</div>
-
-
-</footer>
-    </>
-  )
+  return {
+    props: {
+      posts,
+    },
+  }
 }
+
+export default function Component({ posts }) {
+  
+  const { data: session } = useSession();
+  const router = useRouter()
+
+const handleDelete = async (id) => {
+   //console.log("ID : ", id);
+fetch('https://6fc7-184-82-26-219.ngrok-free.app/api/users?id=' + id, {
+  method: 'DELETE',
+})
+return router.reload('/dashboard')
+}
+  //if (session) {
+    return (
+      <>
+
+{/* <nav className="navbar navbar-light bg-warning">
+  <div className="container-fluid">
+  <div className="col">
+  <div align="right"> Signed in as &nbsp;&nbsp;{session.user.email}&nbsp;&nbsp; {session.user.fname}&nbsp; {session.user.lname} <button  className="btn btn-danger" onClick={() => signOut()}>Sign out</button></div>
+  </div>
+  </div>
+</nav> */}
+<br></br>
+
+        <div className="container">
+          <div className="row">
+          </div>
+          <div align="right">  <Link className="btn btn-success text-n" href="dashboard/frmuser"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                                                                          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                                                          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                                                                          </svg>&nbsp;&nbsp;Add Data</Link> </div>
+          <br></br>
+          <div className="row">
+            <div className="col">
+            <div className="card">
+        <ul className="list-group list-group-flush">
+        <li className="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+                                    <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
+                                    </svg> &nbsp;&nbsp;List member</li>
+        </ul>
+              <table className="table table-striped">
+                <thead>
+                  <tr className="bg-warning">
+                  <th>No</th>
+                    <th>Student ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                    <th>Password</th>
+                    <th>status</th>
+                    <th>Action</th> {/* เพิ่มคอลัมน์ Action */}
+                  </tr>
+                </thead>
+                <tbody>
+                  {posts.users.map((post, i) => (
+                    <tr key={post.id}>
+                      <td>{i+1}</td>
+                      <td>{post.studentid}</td>
+                      <td>{post.firstname}</td>
+                      <td>{post.lastname}</td>
+                      <td>{post.username}</td>
+                      <td>{post.password}</td>
+                      <td>{post.status}</td>
+                      <td>
+                        <button className="btn btn-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                                            </svg></button> &nbsp;&nbsp;
+                        <button className="btn btn-danger" onClick={() => handleDelete(post.id)}> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+                                                           <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+                                                           </svg></button> 
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+
+//   return (
+//     <>
+//       Not signed in <br />
+//       <button onClick={() => signIn()}>Sign in</button>
+//     </>
+//   )
+// }
